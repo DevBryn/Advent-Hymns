16bf9b72879e3c5b1eff466f7a0f0698b0f822eb
Bryan Salonga <winstonbryan01@gmail.com>
2024-12-25 14:33:01 +0800

    Card UI and Minor Responsiveness Adjustments

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
-export default function Card({ children }) {
+export default function Card({ children, link }) {
   return (
-    <div className="bg-primary-green p-6 rounded-xl shadow-lg cursor-pointer">
-      {children}
-    </div>
+    <Link href={link}>
+      <div className="bg-primary-green p-6 rounded-xl shadow-lg cursor-pointer hover:bg-primary-green/80 transition-colors">
+        {children}
+      </div>
+    </Link>
   );
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,11 +4,13 @@ import logo from "@/assets/sda-logo.png";
 import SearchBox from "@/components/SearchBox";
 import Card from "@/components/Card";
 import { MdQueueMusic } from "react-icons/md";
+import { MdOutlinePlayCircle } from "react-icons/md";
+import { MdInfoOutline } from "react-icons/md";
 
 export default function Hero() {
   return (
-    <section className="pt-8">
-      <div className="container">
+    <section className="py-8">
+      <div className="container sm:max-w-[700px] sm:m-auto">
         <div className="flex flex-col gap-8">
           <div className="flex flex-col">
             <div className="flex justify-center items-center">
@@ -18,17 +20,19 @@ export default function Hero() {
               <h1 className="font-bold text-center text-2xl text-transparent bg-clip-text bg-gradient-to-r from-white to-accent-yellow/60">
                 Advent Hymns
               </h1>
-              <p className="text-center font-light text-sm">
+              <p className="text-center font-light text-sm sm:max-w-[400px] sm:m-auto">
                 A comprehensive app dedicated to providing easy access to the
                 cherished hymnals of the Seventh-day Adventist community.
               </p>
             </div>
           </div>
           <div className="flex flex-col">
-            <p className="text-center font-medium mb-2">Quick Search</p>
+            <p className="text-center sm:text-left font-medium mb-2">
+              Quick Search
+            </p>
             <SearchBox />
           </div>
-          <div className="flex flex-col gap-4 mt-2">
+          <div className="flex flex-col sm:flex-row sm:justify-between gap-4 mt-2">
             <Card
               children={
                 <>
@@ -37,10 +41,10 @@ export default function Hero() {
                       <MdQueueMusic className="size-8 text-accent-yellow" />
                     </div>
                     <div>
-                      <h1 className="font-medium">SDA Hymn List</h1>
+                      <h1 className="font-medium sm:text-sm">SDA Hymn List</h1>
                     </div>
                     <div>
-                      <p className="text-sm">
+                      <p className="text-sm sm:text-xs font-light">
                         With over{" "}
                         <span className="font-medium text-accent-yellow">
                           500
@@ -51,52 +55,59 @@ export default function Hero() {
                   </div>
                 </>
               }
+              link={"songs"}
             />
             <Card
               children={
                 <>
                   <div className="flex flex-col gap-2">
                     <div>
-                      <MdQueueMusic className="size-8 text-accent-yellow" />
+                      <MdOutlinePlayCircle className="size-8 text-accent-yellow" />
                     </div>
                     <div>
-                      <h1 className="font-medium">SDA Hymn List</h1>
+                      <h1 className="font-medium sm:text-sm">
+                        SDA Hymn Player
+                      </h1>
                     </div>
                     <div>
-                      <p className="text-sm">
-                        With over{" "}
+                      <p className="text-sm sm:text-xs font-light">
+                        Play any SDA Hymnal with <br />{" "}
                         <span className="font-medium text-accent-yellow">
-                          500
-                        </span>{" "}
-                        SDA hymnal <br /> songs to play.
+                          lyrics
+                        </span>
+                        .
                       </p>
                     </div>
                   </div>
                 </>
               }
+              link={"player"}
             />
             <Card
               children={
                 <>
                   <div className="flex flex-col gap-2">
                     <div>
-                      <MdQueueMusic className="size-8 text-accent-yellow" />
+                      <MdInfoOutline className="size-8 text-accent-yellow" />
                     </div>
                     <div>
-                      <h1 className="font-medium">SDA Hymn List</h1>
+                      <h1 className="font-medium sm:text-sm">
+                        About Advent Hymns
+                      </h1>
                     </div>
                     <div>
-                      <p className="text-sm">
-                        With over{" "}
+                      <p className="text-sm sm:text-xs font-light">
+                        Learn more about <br />{" "}
                         <span className="font-medium text-accent-yellow">
-                          500
+                          Advent Hymns
                         </span>{" "}
-                        SDA hymnal <br /> songs to play.
+                        app.
                       </p>
                     </div>
                   </div>
                 </>
               }
+              link={"about"}
             />
           </div>
         </div>

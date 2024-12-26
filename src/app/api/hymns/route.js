1329c002e3d0dd55c7ff7c8655d208750cd42606
Bryan Salonga <winstonbryan01@gmail.com>
2024-12-26 14:49:57 +0800

    Add hymns POST route

--- src/app/api/hymns/route.js
+++ src/app/api/hymns/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import connect from "../../../../lib/db";
 import Hymn from "../../../../lib/models/hymns";
 
+//get all hymns
 export const GET = async () => {
   try {
     await connect();
@@ -10,6 +11,26 @@ export const GET = async () => {
     return new NextResponse(JSON.stringify(hymns), { status: 200 });
   } catch (error) {
     return new NextResponse("Error fetching hymns: " + error.message, {
+      status: 500,
+    });
+  }
+};
+
+//add hymns
+export const POST = async (request) => {
+  try {
+    const body = await request.json();
+    await connect();
+
+    const newHymn = new Hymn(body);
+    await newHymn.save();
+
+    return new NextResponse(
+      JSON.stringify({ message: "Hymn added - ", hymn: newHymn }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new NextResponse("Error adding hymns: " + error.message, {
       status: 500,
     });
   }

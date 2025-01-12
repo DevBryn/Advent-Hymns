959864dbc912d49c4b79d350c032d5092b8019eb
Bryan Salonga <winstonbryan01@gmail.com>
2025-01-12 16:49:11 +0800

    Added Actions Per Hymn Row

--- src/app/songs/columns.jsx
+++ src/app/songs/columns.jsx
@@ -1,17 +1,49 @@
 "use client";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 // This is the shape of our data.
 export const columns = [
   {
-    accessorKey: "name",
-    header: "Name test",
+    accessorKey: "hymn_number",
+    header: "Hymn no.",
   },
   {
-    accessorKey: "email",
-    header: "Email",
+    accessorKey: "hymn_title",
+    header: "Title",
   },
   {
-    accessorKey: "createdAt",
-    header: "Created",
+    id: "actions",
+    cell: ({ row }) => {
+      //current row
+      const hymnRow = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => console.log(hymnRow.hymn_title)}>
+              Play
+            </DropdownMenuItem>
+            <DropdownMenuItem>Add to favorites</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
--- src/app/songs/page.js
+++ src/app/songs/page.js
@@ -4,9 +4,7 @@ import SearchBox from "@/components/SearchBox";
 
 async function getData() {
   // Fetch data from your API here.
-  const res = await fetch(
-    "https://677a2824671ca03068333314.mockapi.io/api/v1/hymns"
-  );
+  const res = await fetch("http://localhost:3000/api/hymns");
   const data = await res.json();
   return data;
 }
@@ -15,7 +13,7 @@ export default async function page() {
   const data = await getData();
 
   return (
-    <div className="container mx-auto py-10 flex flex-col gap-4">
+    <div className="container sm:max-w-[700px] sm:m-auto py-10 flex flex-col gap-4">
       <div>
         <SearchBox />
       </div>
